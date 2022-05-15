2baabfb320589c25b6aba9d51d9e5800bea45e69
DaryaRamirez <33dramirez@gmail.com>
2022-05-14 19:39:14 -0700

    2nd commit

--- src/HW.java
+++ src/HW.java
@@ -3,5 +3,9 @@ public class HW {
         for (int x=1;x<=100;x++){
             System.out.print(x+", ");
         }
+        System.out.println();
+        for (int x=1;x<=50;x++){
+            System.out.print(x+", ");
+        }
     }
 }
